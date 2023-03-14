10f640256f0bb1bb1e104fe607dfdf98970ceba1
ToniMBIX <antmarbou@alu.edu.gva.es>
2023-03-14 23:05:55 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa una cadena de caracteres: ");
+        string chain = Console.ReadLine();
+
+        chain = chain.Replace(" ", "").ToLower();
+         
+        string reverse = new string(chain.Reverse().ToArray());
+        if (chain == reverse)
+        {
+            Console.WriteLine("La cadena es un palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine("La cadena no es un palíndromo.");
+        }
+    }
+}
